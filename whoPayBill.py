d2432cd72e56ffbbe85e63be88c90427204ee6d4
Shagun <shagunpadha108@gmail.com>
2025-11-23 13:37:05 +0530

    Add files via upload

---
+++ whoPayBill.py
@@ -0,0 +1,17 @@
+import random
+friends = ["shagun","bhoomija","devjeet","divya"]
+# # ist option
+# random = random.randint(1,4)
+# if random == 1:
+#     print("shagun")
+# elif random == 2:
+#     print("Bhoomija")
+# elif random == 3:
+#     print("Devjeet")
+# else:
+#     print("diya")
+
+
+print(friends[random.randint(1,3)]) 
+# # 2nd option
+# print(random.choice(friends))
